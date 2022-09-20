250a1208679e7b3d30f74416a070858295ae5878
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-09-20 04:45:34 -0700

    hero section

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import './header.css'
+import Logo from '../../images/logo.png'
+
+function Header() {
+  return (
+    <div className='header' >
+      <div className="header__logo-container">
+        <img src={Logo} className='header__logo' alt="Logo" />
+      </div>
+      <div className="header__text-box">
+
+      <h1 className="heading-primary">
+        <span className="heading-primary--main">RANGAMATI</span>
+        <span className="heading-primary--sub">is where life happens</span>
+      </h1>
+        <a className='btn btn--white animated--btn' href="/">Discover Now</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
