a3105823420f4e3bf9f948ebb30d4dd5a722dfab
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-09-29 06:49:41 -0700

    card

--- src/Components/Tours.jsx
+++ src/Components/Tours.jsx
@@ -13,7 +13,9 @@ function Tours() {
           <div className="card">
             <div className="card__side card__side--front">
               <div className="card__picture card__picture-1"> &nbsp; </div>
-              <div className="card__heading">heding</div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span-1">Explore The mountain </span>
+              </h4>
               <div className="card__details">details</div>
             </div>
             <div className="card__side card__side--back card__side--back-1">
