961d0bcb0b5921007cd9b7e1c2bae7ba2d407526
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-09-30 23:07:40 -0700

    card all

--- src/Components/Tours.jsx
+++ src/Components/Tours.jsx
@@ -5,7 +5,7 @@ function Tours() {
     <section className="section-tours">
       <div className="u-center-text  u-mb-2">
         <h2 className="heading-secondary">
-          Adventure to the exciting culture of mountain
+          Adventure to the exciting culture of nature
         </h2>
       </div>
       <div className="row-tours">
@@ -14,14 +14,14 @@ function Tours() {
             <div className="card__side card__side--front">
               <div className="card__picture card__picture-1"> &nbsp; </div>
               <h4 className="card__heading">
-                <span className="card__heading-span card__heading-span-1">Explore The mountain </span>
+                <span className="card__heading-span card__heading-span-1">Explore The furmon  </span>
               </h4>
               <div className="card__details">
                 <ul>
-                  <li>7 day tour</li>
+                  <li>1 day tour</li>
                   <li>Up to 15 people</li>
                   <li>2 tour guides</li>
-                  <li>Sleep in cozy hotel</li>
+                  <li>no sleep</li>
                   <li>Difficulity: easy</li>
                 </ul>
               </div>
@@ -37,8 +37,62 @@ function Tours() {
             </div>
           </div>
         </div>
-        <div className="col-1-of-3">1</div>
-        <div className="col-1-of-3">1</div>
+        <div className="col-1-of-3">
+           <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture-2"> &nbsp; </div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span-2">dive into kaptai lake</span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>7 day tour</li>
+                  <li>Up to 15 people</li>
+                  <li>2 tour guides</li>
+                  <li>Sleep in cozy hotel</li>
+                  <li>Difficulity: easy</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card__side card__side--back card__side--back-2">
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">2000</p>
+                </div>
+                <a href="#/" className="btn btn--white" >Book Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-1-of-3">
+           <div className="card">
+            <div className="card__side card__side--front">
+              <div className="card__picture card__picture-3"> &nbsp; </div>
+              <h4 className="card__heading">
+                <span className="card__heading-span card__heading-span-3">Explore Buddha behar </span>
+              </h4>
+              <div className="card__details">
+                <ul>
+                  <li>7 day tour</li>
+                  <li>Up to 15 people</li>
+                  <li>2 tour guides</li>
+                  <li>Sleep in cozy hotel</li>
+                  <li>Difficulity: easy</li>
+                </ul>
+              </div>
+            </div>
+            <div className="card__side card__side--back card__side--back-3">
+              <div className="card__cta">
+                <div className="card__price-box">
+                  <p className="card__price-only">Only</p>
+                  <p className="card__price-value">1200</p>
+                </div>
+                <a href="#/" className="btn btn--white" >Book Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
