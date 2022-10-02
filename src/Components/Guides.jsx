7c8cf6fcaeace2aa26a233ea605384e82f60368e
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-10-01 23:29:12 -0700

    guides backgroung video

--- src/Components/Guides.jsx
+++ src/Components/Guides.jsx
@@ -1,53 +1,85 @@
 import React from "react";
-import guid1 from '../images/naruto.jpg'
-import kakashi from '../images/kakashi.jpg'
-import hinata from '../images/hinata.jpg'
+import guid1 from "../images/naruto.jpg";
+import kakashi from "../images/kakashi.jpg";
+import hinata from "../images/hinata.jpg";
+import video from '../images/nature.mp4'
 
 function Guides() {
   return (
     <section className="section-guides">
+      <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop>
+          <source src={video} />
+          your browser dos not support this video
+        </video>
+      </div>
       <div className="u-center-text  u-mb-2 u-mt-8">
         <h2 className="heading-secondary">Guides or superheros</h2>
       </div>
       <div className="row">
         <div className="guide">
           <figure className="guide__shape">
-            <img src={guid1} className='guide__img' alt="" />
+            <img src={guid1} className="guide__img" alt="" />
             <figcaption className="guide__caption">Naruto Uzumaki</figcaption>
           </figure>
           <div className="guide__text">
-            <h3 className="heading-tertiary u-mb-2">I want to be the best ninja of all time</h3>
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod assumenda perspiciatis quo alias velit mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cupiditate quidem ea? Labore, id facilis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa numquam ab dolore cumque natus?</p>
+            <h3 className="heading-tertiary u-mb-2">
+              I want to be the best ninja of all time
+            </h3>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              assumenda perspiciatis quo alias velit mollitia. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Illum cupiditate quidem ea?
+              Labore, id facilis! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Rem ipsa numquam ab dolore cumque natus?
+            </p>
           </div>
         </div>
       </div>
       <div className="row">
         <div className="guide">
           <figure className="guide__shape">
-            <img src={kakashi} className='guide__img' alt="" />
-            <figcaption className="guide__caption">Kakashi Hatake
-</figcaption>
+            <img src={kakashi} className="guide__img" alt="" />
+            <figcaption className="guide__caption">Kakashi Hatake</figcaption>
           </figure>
           <div className="guide__text">
-            <h3 className="heading-tertiary u-mb-2">I want to be the best ninja of all time</h3>
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod assumenda perspiciatis quo alias velit mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cupiditate quidem ea? Labore, id facilis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa numquam ab dolore cumque natus?</p>
+            <h3 className="heading-tertiary u-mb-2">
+              I want to be the best ninja of all time
+            </h3>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              assumenda perspiciatis quo alias velit mollitia. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Illum cupiditate quidem ea?
+              Labore, id facilis! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Rem ipsa numquam ab dolore cumque natus?
+            </p>
           </div>
         </div>
       </div>
       <div className="row">
         <div className="guide">
           <figure className="guide__shape">
-            <img src={hinata} className='guide__img' alt="" />
+            <img src={hinata} className="guide__img" alt="" />
             <figcaption className="guide__caption">Hinata Hyuga</figcaption>
           </figure>
           <div className="guide__text">
-            <h3 className="heading-tertiary u-mb-2">I want to be the best ninja of all time</h3>
-            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod assumenda perspiciatis quo alias velit mollitia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cupiditate quidem ea? Labore, id facilis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem ipsa numquam ab dolore cumque natus?</p>
+            <h3 className="heading-tertiary u-mb-2">
+              I want to be the best ninja of all time
+            </h3>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              assumenda perspiciatis quo alias velit mollitia. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Illum cupiditate quidem ea?
+              Labore, id facilis! Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Rem ipsa numquam ab dolore cumque natus?
+            </p>
           </div>
         </div>
       </div>
       <div className="u-center-text  u-mt-8">
-        <a href="/" className="btn-text">View more Ninjas as guides &rarr; </a>
+        <a href="/" className="btn-text">
+          View more Ninjas as guides &rarr;{" "}
+        </a>
       </div>
     </section>
   );
