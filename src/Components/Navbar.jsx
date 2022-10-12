2d8abbfbbf528cd0e003e533aa17cd886fcb518d
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2022-10-12 10:10:41 -0700

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 function Navbar() {
+  const [navOpen,setNavOpen] = useState(false)
   return (
+
     <section className="nav-section">
       <div className="nav_container">
         <div className="navbar">
           <div className="navbar-logo">RGT-ISM</div>
-          <div className="menu-toggle">
-            <div className="hamBox">
+          <div className="menu-toggle" onClick={()=> setNavOpen(!navOpen)}>
+            <div className={navOpen ? 'hamBox hamBoxOpen': 'hamBox'}>
               <span className="lineTop"></span>
               <span className="lineBottom"></span>
             </div>
